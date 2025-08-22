b1096ca072d0710509461c1b61de4bc4d112d1e9
Антон <anton@MacBook-Air-Anton.local>
2025-08-22 14:27:46 +0300

    feat: add form validation

--- src/js/main.js
+++ src/js/main.js
@@ -35,6 +35,50 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
-    
+    /* ---------- Form ---------- */
+
+    document.querySelector('.contacts-form__button').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        let resultMessage = document.querySelector('.contacts-form__result'),
+            checkbox = document.querySelector('.contacts-form-checkbox__input'),
+            fieldsOK = false,
+            checkboxOK = false;
+
+        document.querySelectorAll('.contacts-form__field').forEach(function (element) {
+            if (element.value.trim() === '') {
+                element.classList.add('empty-field');
+                resultMessage.classList.add('error-message');
+                resultMessage.textContent = 'Пожалуйста, заполните обязательные поля';
+            } else {
+                fieldsOK = true;
+            }
+        });
+
+        if (!checkbox.checked) {
+            resultMessage.classList.add('error-message');
+            resultMessage.textContent = 'Пожалуйста, отметьте согласие с обработкой данных';
+        } else {
+            checkboxOK = true;
+        }
+
+        if (fieldsOK && checkboxOK) {
+            resultMessage.classList.add('success-message');
+            resultMessage.textContent = 'Спасибо! Ваше сообщение успешно отправлено';
+
+            document.querySelectorAll('.contacts-form__field').forEach(function (element) {
+                element.value = '';
+            });
+        }
+    });
+
+    document.querySelectorAll('.contacts-form__field').forEach(function (field) {
+        field.addEventListener('click', function () {
+            if (this.classList.contains('empty-field')) {
+                this.classList.remove('empty-field');
+            }
+        });
+    });
+
 
 });
